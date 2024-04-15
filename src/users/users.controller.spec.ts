ed6d4e90589cf6cd8625b7abb3a6206622170910
Pedro Ramos 0 <pedroheramos@gmail.com>
2024-04-15 12:36:26 -0300

    users.controler.spec root

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './service/users.service';
+import { UsersRepository } from '../auth/auth.controller.spec';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './entity/user.entity';
+import { Helper } from '../app.helper';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
+  let helper: Helper;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: UsersRepository
+        },
+      Helper
+    ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    helper = await module.resolve(Helper);
   });
 
   it('should be defined', () => {
