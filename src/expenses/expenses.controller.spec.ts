c6444d415ae537a6db2f683147ca9902ec86bd08
Pedro Ramos 0 <pedroheramos@gmail.com>
2024-04-15 18:17:09 -0300

    expenses.controller & expenses.service.spec

--- src/expenses/expenses.controller.spec.ts
+++ src/expenses/expenses.controller.spec.ts
@@ -1,15 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExpensesController } from './expenses.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Helper } from '../app.helper';
+import { ExpensesService } from './service/expenses.service';
+import { Expenses } from './entity/expenses.entity';
+import { ExpensesRepository } from './service/expenses.service.spec';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from '../users/entity/user.entity';
+import { UsersRepository } from '../auth/auth.controller.spec';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 describe('ExpensesController', () => {
   let controller: ExpensesController;
+  let jwtService: JwtService;
+  let helper: Helper;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MailerModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async(configService: ConfigService) => ({
+            transport: {
+              host: configService.get('MAIL_HOST'),
+              port: configService.get('MAIL_PORT'),
+              secure: true, // upgrade later with STARTTLS
+              auth: {
+                user: configService.get('MAIL_USER'),
+                pass: configService.get('MAIL_PASS'),
+              },
+            },
+            defaults: {
+              from:'"nest-modules" <modules@nestjs.com>',
+            },
+            template: {
+              dir: process.cwd() + '/templates/',
+              adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+              options: {
+                strict: true,
+              },
+            },
+          }),
+          inject: [ConfigService],
+        }),
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get('CONSTANT_SECRET_ENCRYPTER'),
+            signOptions: {
+                expiresIn: 3600,
+            },
+          }),
+          inject: [ConfigService],
+          global: true
+        }),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        })
+
+      ],
+      providers: [
+        ExpensesService,
+        Helper,
+        {
+          provide: getRepositoryToken(Expenses),
+          useClass: ExpensesRepository
+        },
+        {
+          provide: getRepositoryToken(Users),
+          useClass: UsersRepository
+        }
+      ],
       controllers: [ExpensesController],
     }).compile();
 
     controller = module.get<ExpensesController>(ExpensesController);
+    jwtService = module.get<JwtService>(JwtService);
+    helper = await module.resolve(Helper);
   });
 
   it('should be defined', () => {
--- src/expenses/service/expenses.service.spec.ts
+++ src/expenses/service/expenses.service.spec.ts
@@ -1,15 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExpensesService } from './expenses.service';
+import { UsersRepository } from '../../auth/auth.controller.spec';
+import { Helper } from '../../app.helper';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from '../../users/entity/user.entity';
+import { Expenses } from '../entity/expenses.entity';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { JwtService } from '@nestjs/jwt';
+
+
+export const ExpensesRepository = jest.fn(() => ({
+  metadata: {
+    columns: [],
+    relations: [],
+  },
+}));
 
 describe('ExpensesService', () => {
   let service: ExpensesService;
+  let mailerService: MailerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExpensesService],
+      imports: [
+        MailerModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async(configService: ConfigService) => ({
+            transport: {
+              host: configService.get('MAIL_HOST'),
+              port: configService.get('MAIL_PORT'),
+              secure: true, // upgrade later with STARTTLS
+              auth: {
+                user: configService.get('MAIL_USER'),
+                pass: configService.get('MAIL_PASS'),
+              },
+            },
+            defaults: {
+              from:'"nest-modules" <modules@nestjs.com>',
+            },
+            template: {
+              dir: process.cwd() + '/templates/',
+              adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+              options: {
+                strict: true,
+              },
+            },
+          }),
+          inject: [ConfigService],
+        })
+      ],
+      providers: [
+        ExpensesService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: UsersRepository
+        },
+        {
+          provide: getRepositoryToken(Expenses),
+          useClass: ExpensesRepository
+        },
+        Helper
+      ]
     }).compile();
 
     service = module.get<ExpensesService>(ExpensesService);
+    mailerService = module.get(MailerService);
   });
 
   it('should be defined', () => {
